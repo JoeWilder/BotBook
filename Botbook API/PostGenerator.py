05e34bf5d9b97401480f823dd9c2e0f950d75bee
JoeWilder <josephwilder21@gmail.com>
2023-10-31 13:08:03 -0400

    Add a comment generator function

--- Botbook API/PostGenerator.py
+++ Botbook API/PostGenerator.py
@@ -12,7 +12,7 @@ def GeneratePost(personalityPrompt, postPrompt):
       },
       {
         "role": "user",
-        "content": postPrompt
+        "content": "Make a post about " + postPrompt
       }
     ],
     temperature=0.8,
@@ -25,4 +25,31 @@ def GeneratePost(personalityPrompt, postPrompt):
   return response.choices[0].message.content
 
 
-print(GeneratePost("-passionate\n-excited", "Make a post about favorite mario character"))
+def GenerateComment(personalityPrompt, post):
+  openai.api_key = os.getenv("ChatGPT")
+  response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {
+        "role": "system",
+        "content": "Use the following principles to leave a comment on a social media post:\n\n- " + personalityPrompt
+      },
+      {
+        "role": "user",
+        "content": "Make a comment about the following post:: " + post
+      }
+    ],
+    temperature=0.8,
+    max_tokens=500,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
+
+  return response.choices[0].message.content
+
+
+post = GeneratePost("-passionate\n-excited", "your favorite mario character")
+print(post + "\n\n\n")
+print(GenerateComment("-tired\n-supportive", post) + "\n\n\n")
+print(GenerateComment("-angry\n-annoyed", post) + "\n\n\n")
